a54ced3313f960ef7689cc7cb08592dbfb309228
Morelcia <nat@nekopon.pl>
2021-09-02 11:12:40 +0200

    wtf gedit

--- site/js/weather.js
+++ site/js/weather.js
@@ -1,35 +1,35 @@
 class Weather {
-	constructor() {
-	  this._apiKey = 'yea key goes here';
-	  this._city = 'Warszawa, PL';
-		this._apiEndpoint = 'https://api.openweathermap.org/data/2.5/weather';
-		this._divDOM = document.querySelector('.weather');
-		this._iconDOM = document.querySelector('.weather-icon');
+  constructor() {
+    this._apiKey = 'yea key goes here';
+    this._city = 'Warszawa, PL';
+    this._apiEndpoint = 'https://api.openweathermap.org/data/2.5/weather';
+    this._divDOM = document.querySelector('.weather');
+    this._iconDOM = document.querySelector('.weather-icon');
     this._cityDOM = document.querySelector('.weather-city');
     this._tempDOM = document.querySelector('.weather-temperature');
     this._descDOM = document.querySelector('.weather-description');
-		this._init();
-	}
+    this._init();
+  }
 	
-	async _fetchWeatherApi() {
+  async _fetchWeatherApi() {
     const response = await fetch(this._apiEndpoint + '?units=metric&q=' + this._city + '&appid=' + this._apiKey);
     const json = await response.json();
     return json;
   }
 
-	async _displayWeather() {
-	  this._currWeather = await this._fetchWeatherApi();
-	  console.log(this._currWeather);
-	  if (this._currWeather.cod == '200') {
-	    this._iconDOM.setAttribute('src', 'https://openweathermap.org/img/wn/' + this._currWeather.weather[0].icon + '@2x.png');
-	    this._cityDOM.innerText = this._currWeather.name;
-	    this._tempDOM.innerHTML = this._currWeather.main.temp + '<b>°C</b>';
-	    this._descDOM.innerText = this._currWeather.weather[0].description;
-	    this._divDOM.style.display = 'flex';
-	  }
-	}
+  async _displayWeather() {
+    this._currWeather = await this._fetchWeatherApi();
+    console.log(this._currWeather);
+    if (this._currWeather.cod == '200') {
+      this._iconDOM.setAttribute('src', 'https://openweathermap.org/img/wn/' + this._currWeather.weather[0].icon + '@2x.png');
+      this._cityDOM.innerText = this._currWeather.name;
+      this._tempDOM.innerHTML = this._currWeather.main.temp + '<b>°C</b>';
+      this._descDOM.innerText = this._currWeather.weather[0].description;
+      this._divDOM.style.display = 'flex';
+    }
+  }
 
-	_init() {
-		this._displayWeather();
-	}
+  _init() {
+    this._displayWeather();
+  }
 }
